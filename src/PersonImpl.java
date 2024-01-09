dba1acc39690e94557f836726e6ebc3bc058fd8b
Nurlan <nurlanabijbillaev@gmail.com>
2024-01-09 15:41:26 +0600

    feature/addnewmethods

---
+++ src/PersonImpl.java
@@ -0,0 +1,9 @@
+public class PersonImpl {
+    public void addNewPerson(){
+        System.out.println("I can added new person");
+    } public void updatePerson(){
+        System.out.println("I can update person");
+    } public void deletePerson(){
+        System.out.println("I can delete person");
+    }
+}
