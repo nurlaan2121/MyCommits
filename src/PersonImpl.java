beaa4e1ea02a4b11f444de956e91f4b7fe439602
Nurlan <nurlanabijbillaev@gmail.com>
2024-01-09 15:44:57 +0600

    refactor/PersomImpl/addmethod

--- src/PersonImpl.java
+++ src/PersonImpl.java
@@ -1,9 +1,20 @@
 public class PersonImpl {
-    public void addNewPerson(){
+    public void addNewPerson() {
         System.out.println("I can added new person");
-    } public void updatePerson(){
+    }
+
+    public void updatePerson() {
         System.out.println("I can update person");
-    } public void deletePerson(){
+    }
+
+    public void deletePerson() {
+        System.out.println("I can 42 person");
         System.out.println("I can delete person");
+        System.out.println("I can derwewrlete person");
+        System.out.println("I can delewte person");
+        System.out.println("I can delegdftfde person");
+        System.out.println("I can deledfgte person");
+        System.out.println("I can delegdfte person");
+
     }
 }
